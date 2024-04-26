88485d303ce7fe5004fc4f1d9cac2d157acaea13
Mark <marklajom.work@gmail.com>
2024-04-26 16:30:34 +0200

    add loader for global renders

--- vite-project/src/features/menu/Menu.jsx
+++ vite-project/src/features/menu/Menu.jsx
@@ -1,5 +1,20 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+  return (
+    <div>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </div>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
--- vite-project/src/features/menu/MenuItem.jsx
+++ vite-project/src/features/menu/MenuItem.jsx
@@ -1,4 +1,5 @@
-import { formatCurrency } from "../../utility/helpers";
+/* eslint-disable react/prop-types */
+import { formatCurrency } from "../../utils/helpers";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
