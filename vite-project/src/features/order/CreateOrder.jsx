71d3e95578b354877508e24693d28ca903a8b9ed
Mark <marklajom.work@gmail.com>
2024-04-29 18:23:14 +0200

    add tailwind styles

--- vite-project/src/features/order/CreateOrder.jsx
+++ vite-project/src/features/order/CreateOrder.jsx
@@ -1,32 +1,32 @@
 /* eslint-disable react/no-unescaped-entities */
-import { useState } from "react";
-import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-import { createOrder } from "../../services/apiRestaurant";
+import { useState } from 'react';
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
+import { createOrder } from '../../services/apiRestaurant';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -35,7 +35,7 @@ const fakeCart = [
 
 function CreateOrder() {
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === "submitting";
+  const isSubmitting = navigation.state === 'submitting';
 
   const formErrors = useActionData();
   // const [withPriority, setWithPriority] = useState(false);
@@ -79,8 +79,11 @@ function CreateOrder() {
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           {formErrors?.phone && <p>{formErrors.phone}</p>}
-          <button disabled={isSubmitting}>
-            {isSubmitting ? "Placing order.." : "Order now"}
+          <button
+            disabled={isSubmitting}
+            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-all duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+          >
+            {isSubmitting ? 'Placing order..' : 'Order now'}
           </button>
         </div>
       </Form>
@@ -95,12 +98,12 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on",
+    priority: data.priority === 'on',
   };
 
   const errors = {};
   if (!isValidPhone(order.phone))
-    errors.phone = "Please give us your correct phone number.";
+    errors.phone = 'Please give us your correct phone number.';
 
   if (Object.keys(errors).length > 0) return errors;
   const newOrder = await createOrder(order);
--- vite-project/src/features/order/SearchOrder.jsx
+++ vite-project/src/features/order/SearchOrder.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function SearchOrder() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   const navigate = useNavigate();
 
   function handleSubmit(e) {
@@ -10,7 +10,7 @@ function SearchOrder() {
     if (!query) return;
 
     navigate(`/order/${query}`);
-    setQuery("");
+    setQuery('');
   }
 
   return (
@@ -19,6 +19,7 @@ function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:w-72 sm:w-64"
       />
     </form>
   );
