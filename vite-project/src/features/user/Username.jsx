fb78e99a3ccc1a18644af42ec3f907c9949c4b1c
Mark <marklajom.work@gmail.com>
2024-04-30 16:17:18 +0200

    add react redux

--- vite-project/src/features/user/Username.jsx
+++ vite-project/src/features/user/Username.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Mark</div>;
+  const userName = useSelector((state) => state.user.username);
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{userName}</div>
+  );
 }
 
 export default Username;
